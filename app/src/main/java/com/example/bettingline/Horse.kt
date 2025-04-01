e33833896cd62a93a3f3e4526feebab66a07e58d
mltieman <mltieman@email.sc.edu>
2025-04-01 14:56:36 -0400

    added screens to pull

--- app/src/main/java/com/example/bettingline/Horse.kt
+++ app/src/main/java/com/example/bettingline/Horse.kt
@@ -1,16 +1,2 @@
 package com.example.bettingline
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.ui.graphics.Color
-import kotlin.random.Random
-
-data class Horse(
-    val name: String,
-    val speed: Float,
-    val stamina: Float,
-    val luck: Float,  // 0.0 (unlucky) to 1.0 (very lucky)
-    val experience: Float,
-    val progress: MutableState<Float> = mutableFloatStateOf(0f),
-    val color: Color = Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat())
-)
